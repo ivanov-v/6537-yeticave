a05668d5111f7b785bb93a65a3c7c9e42160d9c0
ivanov-v <sferamail33@yandex.ru>
2017-11-10 17:11:28 +0300

    Сделан вывод меню юзера

--- index.php
+++ index.php
@@ -41,9 +41,23 @@ $now = strtotime('now');
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
-
+            <?php if ($is_auth): ?>
+                <div class="user-menu__image">
+                    <img src="<?=$user_avatar?>" width="40" height="40" alt="Пользователь">
+                </div>
+                <div class="user-menu__logged">
+                    <p><?=$user_name?></p>
+                </div>
+            <?php else: ?>
+                <ul class="user-menu__list">
+                    <li class="user-menu__item">
+                        <a href="#">Регистрация</a>
+                    </li>
+                    <li class="user-menu__item">
+                        <a href="#">Вход</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
