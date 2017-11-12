bc7d5738b1b98f69197a7b998d84227e1ba3635b
ivanov-v <sferamail33@yandex.ru>
2017-11-12 15:40:25 +0300

    Сделан вывод лотов

--- index.php
+++ index.php
@@ -145,24 +145,26 @@ function to_decade($num) {
             <h2>Открытые лоты</h2>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            <?=$lot_time_remaining;?>
+            <?php foreach ($lots as $lot): ?>
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="<?=$lot['img']?>" width="350" height="260" alt="<?=$lot['name']?>">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category"><?=$lot['category']?></span>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name']?></a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost"><?=$lot['price']?><b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                <?=$lot_time_remaining;?>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>
+            <?php endforeach; ?>
         </ul>
     </section>
 </main>
