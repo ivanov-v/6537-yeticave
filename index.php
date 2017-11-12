6665eead8f1c7511b16270ff2ba8df29410130b4
ivanov-v <sferamail33@yandex.ru>
2017-11-12 16:00:20 +0300

    Изменено поле имени лота

--- index.php
+++ index.php
@@ -33,37 +33,37 @@ $lot_categories = [
 
 $lots = [
     [
-        'name' => '2014 Rossignol District Snowboard',
+        'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
         'img' => 'img/lot-1.jpg'
     ],
     [
-        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
         'img' => 'img/lot-2.jpg'
     ],
     [
-        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
         'img' => 'img/lot-3.jpg'
     ],
     [
-        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
         'img' => 'img/lot-4.jpg'
     ],
     [
-        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
         'img' => 'img/lot-5.jpg'
     ],
     [
-        'name' => 'Маска Oakley Canopy',
+        'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
         'img' => 'img/lot-6.jpg'
@@ -152,7 +152,7 @@ function to_decade($num) {
                     </div>
                     <div class="lot__info">
                         <span class="lot__category"><?=$lot['category']?></span>
-                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['name']?></a></h3>
+                        <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$lot['title']?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
